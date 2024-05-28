a3dd3e4072e822df835fac4a3f18788d041e8fc2
Kajvd <58260553+Kajvd@users.noreply.github.com>
2024-05-29 00:57:15 +0200

    Delete basiswebsite2 directory

--- basiswebsite2/scripts/script.js
+++
@@ -1,9 +0,0 @@
-var hamburger = document.querySelector(".hamburger");
-var navMenu = document.querySelector(".nav-menu");
-
-hamburger.addEventListener("click", mobileMenu);
-
-function mobileMenu() {
-    hamburger.classList.toggle("active");
-    navMenu.classList.toggle("active");
-}
