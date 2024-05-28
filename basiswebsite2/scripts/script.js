9d7c7b64fece977a4f3201b0e1c7f6072d973fd4
Kajvd <58260553+Kajvd@users.noreply.github.com>
2024-05-29 00:58:09 +0200

    Add files via upload

---
+++ basiswebsite2/scripts/script.js
@@ -0,0 +1,9 @@
+var hamburger = document.querySelector(".hamburger");
+var navMenu = document.querySelector(".nav-menu");
+
+hamburger.addEventListener("click", mobileMenu);
+
+function mobileMenu() {
+    hamburger.classList.toggle("active");
+    navMenu.classList.toggle("active");
+}
